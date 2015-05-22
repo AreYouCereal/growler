639d39e54bcb59d6f0d4ec5da7bafe59808edbd2
Wendi Speciale <Wendi@CreateSalesAndMarketing.com>
2015-05-22 15:20:20 -0500

    updating app file

--- g_app.rb
+++ g_app.rb
@@ -5,11 +5,17 @@ require "sinatra/reloader"
 # get is the METHOD and banana is the PATH . The two of these, METHOD + BANANA = ROUTE
 
 get '/' do
+	growls = CSV.read('growls.csv')
+	@growls_line = growls.each
+
+
 	erb :index
+	
 end
 
 
-
+post '/csv' do
+end
 
 
 
